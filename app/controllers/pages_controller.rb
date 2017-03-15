58285931b4ab2032bb0ccbeade331ed5b1b2bab5
Dan Ellis <de3177@bard.edu>
2017-03-15 21:31:33 +0000

    Finishing Task 6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,39 @@
 class PagesController < ApplicationController
   def home
   end
+  
+def twill
+  require 'twilio-ruby'
+  
+  account_sid= "AC9ace702bb5d2a2e7a1848a8bc03112a8"
+  auth_token = "ea2aaacff1c373fae94835898ba63077"
+  
+  
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message1 = @client.account.messages.create(:body => "Hello from ruby",
+    :to => "7812344622",
+    :from =>"8574073135")
+  
+  puts message1.sid
+  
+end
 
   def sms
     @number = params[:q]
     @message = params[:d]
+  
+  require 'twilio-ruby'
+  
+  account_sid= "AC9ace702bb5d2a2e7a1848a8bc03112a8"
+  auth_token = "ea2aaacff1c373fae94835898ba63077"
+  
+  
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message1 = @client.account.messages.create(:body => @message,
+    :to => @number,
+    :from =>"8574073135")
+  
+  puts message1.sid
+    
   end
 end
